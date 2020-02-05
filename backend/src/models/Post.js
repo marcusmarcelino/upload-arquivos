7606dac26b75ee944476ba69f78809497ce52288
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-02-05 00:54:51 -0300

    Finalização da implementação do backend

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 
 const s3 = new aws.S3();
 
@@ -27,7 +30,9 @@ PostSchema.pre('remove', function() {
       Key: this.key,
     }).promise()
   }else{
-
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, "..", "..", "tmp", "uploads", this.key)
+    );
   }
 });
 
