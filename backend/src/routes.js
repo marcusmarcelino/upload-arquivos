3c041d7fb67ab339106fbaf2f3c7e7e57596a46d
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-02-05 00:03:43 -0300

    Função de upload de imagen no aws 100% implementada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,7 @@ const multerConfig = require("./config/multer");
 const Post = require('./models/Post');
 
 routes.post("/posts", multer(multerConfig).single('file'), async (req, res) => {
-  const { originalname: name, size, key, url = '' } = req.file;
+  const { originalname: name, size, key, location: url = '' } = req.file;
   console.log(req.file);
   const post = await Post.create({
     name,
