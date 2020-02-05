4d3cd7cd2928cbada37120b80488a6a8e2d6b577
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-02-05 00:17:29 -0300

    Funções de listagem e exclusão de arquivo, implementadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,11 @@ const multerConfig = require("./config/multer");
 
 const Post = require('./models/Post');
 
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  return res.json(posts);
+});
+
 routes.post("/posts", multer(multerConfig).single('file'), async (req, res) => {
   const { originalname: name, size, key, location: url = '' } = req.file;
   console.log(req.file);
@@ -15,6 +20,12 @@ routes.post("/posts", multer(multerConfig).single('file'), async (req, res) => {
   });
   console.log(post);
   return res.json(post);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  await post.remove();
+  return res.send();
 });
 
 module.exports = routes;
